feb88b5b17e072b305ae31698d8ca88da3a3f9b1
PC11 <PC11@INFO5-5382>
2022-03-16 09:35:34 +0100

    9.35commit

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -11,20 +11,22 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
         public int Codice { get; private set; }
-        public string Descdrizione { get; private set; }
+        public string Descrizione { get; private set; }
         public string UnitaMisura { get; private set; }
         public double Prezzo { get; private set; }
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
-        public int Codice
-        {
-            get { codice; }
-        }
+        
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            Codice = codice;
+            Descrizione = descrizione;
+            UnitaMisura = unitaMisura;
+        
+        
         }
 
         public string Visualizzati()
