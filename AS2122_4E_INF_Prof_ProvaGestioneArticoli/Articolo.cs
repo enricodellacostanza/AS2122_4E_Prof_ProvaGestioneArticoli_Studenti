54a4b8feef454d341164fdb4dfa6522ab4e676a9
PC11 <PC11@INFO5-5382>
2022-03-16 09:25:07 +0100

    commit del punto8

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,6 +10,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
+        private int codice;
+        private string descdrizione;
+        private string unitaMisura;
+        private double prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
